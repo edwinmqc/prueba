3ae673fd2962a80d8647acba2c28b35931d8681e
Edwin Manuel Quevedo Cardozo <edwinquevedo@gmail.com>
2021-11-09 12:26:30 -0500

    archivos

---
+++ WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Datos : ControllerBase
+    {
+        
+ "ID 001": {
+  "Nombre": "Edwin",
+  "Apellido": "Edwin",
+  "País": "Colombia",
+  "Ciudad": "Bogota",
+   "Documento":"1032382021"
+
+ },
+ 
+
+
+}
+    }
+}
